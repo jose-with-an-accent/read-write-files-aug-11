4edf8a305ee7fe61f7a78593b8ba4cb1c29549a1
Jose Sanchez <josersanchez0117@gmail.com>
2020-08-11 13:41:23 -0500

    Finished

--- FileStorageDemo/ViewController.swift
+++ FileStorageDemo/ViewController.swift
@@ -9,7 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var readTextLabel: UILabel!
+    @IBOutlet var writeTextBox: UITextField!
+    
+    let fm = FileManager.default
+    
+    @IBAction func onWriteButtonPress(_ sender: Any) {
+        let fileURLs = fm.urls(for: .documentDirectory, in: .userDomainMask)
+        let fileURL = fileURLs.last?.appendingPathComponent("file.text")
+        do {
+            try writeTextBox.text?.write(to: fileURL!, atomically: true, encoding: .utf8)
+        } catch {
+            print("Error writing")
+        }
+    }
+    @IBAction func onReadButtonPress(_ sender: Any) {
+        let fileURLs = fm.urls(for: .documentDirectory, in: .userDomainMask)
+        let fileURL = fileURLs.last?.appendingPathComponent("file.text")
+        do {
+            let fileContents = try String(contentsOf: fileURL!, encoding: .utf8)
+            readTextLabel.text = fileContents
+        } catch {
+            print("Error Reading")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
